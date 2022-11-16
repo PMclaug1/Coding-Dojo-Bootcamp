e336cbdc29d878096c616dcbd356a0fdff219646
PMclaug1 <pmclaug1@gmail.com>
2022-11-15 21:36:16 -0600

    updates

---
+++ Web_Fundamentals/Exam Practice 1/script.js
@@ -0,0 +1,9 @@
+
+
+function showChoices(id) {
+    document.querySelector(id).style.visibility = 'visible'
+}
+
+function hideChoices(id) {
+    document.querySelector(id).style.visibility = 'hidden'
+}
