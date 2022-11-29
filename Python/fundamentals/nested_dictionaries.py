858a49c0bdf1625e8403ee88fa582dbaa76bb002
PMclaug1 <pmclaug1@gmail.com>
2022-11-28 19:43:52 -0600

    nested_dictionaries

---
+++ Python/fundamentals/nested_dictionaries.py
@@ -0,0 +1,70 @@
+"""
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+     {'first_name':  'Michael', 'last_name' : 'Jordan'},
+     {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
+}
+z = [ {'x': 10, 'y': 20} ]
+
+x[1][0] = 15
+
+#print(x)
+
+students[0]['last_name'] = "Bryant"
+
+#print(students)
+
+sports_directory['soccer'][0] = "Andres"
+
+#print(sports_directory['soccer'])
+
+z[0]["y"] = 30
+
+#print(z[0]["y"])
+
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary(students):
+    for key in students:
+        print(key) 
+
+iterateDictionary(students)
+
+
+students = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+def iterateDictionary2(students):
+    for val in students:
+        print(val['first_name'])
+
+iterateDictionary2(students)
+"""
+
+dojo = {
+   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+
+
+def printInfo(dojo):
+        print(len(dojo['locations']))
+        print(dojo['locations'])
+        print(len(dojo['instructors']))
+        print(dojo['instructors'])
+
+printInfo(dojo)
