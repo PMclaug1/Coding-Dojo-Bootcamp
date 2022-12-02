729aa27cc330fa7f87f00b3411d7557bcb4c4bcd
PMclaug1 <pmclaug1@gmail.com>
2022-12-01 19:08:14 -0600

    html table

--- Python/fundamentals/playground/server.py
+++ Python/fundamentals/playground/server.py
@@ -1,9 +1,23 @@
-
 from flask import Flask, render_template  
 
 app = Flask(__name__)  
 
 @app.route('/')          
 def playground():
-    return render_template("index.html")
-    # , color="peachpuff")
+    return "Welcome!"
+
+@app.route('/play')
+def play():
+    return render_template("index.html", times=3)
+
+@app.route('/play/<int:times>')
+def play_times(times):
+    return render_template("index.html", times=times)
+
+@app.route('/play/<int:times>/<string:color>')
+def play_times_color(times,color):
+    return render_template("index.html", times=times, color=color)
+
+
+if __name__=="__main__":      
+    app.run(debug=True)
