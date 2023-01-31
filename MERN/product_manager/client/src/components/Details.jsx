a07c1ddb3972b6210e100ca99dd644adb26e27cf
PMclaug1 <pmclaug1@gmail.com>
2023-01-31 08:00:38 -0600

    product

--- MERN/product_manager/client/src/components/Details.jsx
+++ MERN/product_manager/client/src/components/Details.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {Link} from 'react-router-dom'
+import {useNavigate, Link} from 'react-router-dom'
 import axios from 'axios'
 import { useParams } from 'react-router'
 
@@ -7,11 +7,22 @@ const Details = () => {
     const [product, setProduct] = useState("")
     const {id} = useParams()
 
+    const navigate = useNavigate()
+
     useEffect(() => {
         axios.get(`http://localhost:8000/api/product/${id}`)
         .then((res) => setProduct(res.data))
         .catch(err => console.log('This is our details page: ' + err))
     }, [id])
+
+    const handleDelete = (e, id) => {
+        console.log(`Deleting ${id}`, e)
+        axios.delete(`http://localhost:8000/api/product/${id}`)
+        .then(res => {
+            navigate("/")
+        })
+        .catch((err) => console.log(err))
+    }
 
     return (
         <div>
@@ -20,6 +31,7 @@ const Details = () => {
             <p>Description: {product.description}</p>
             <p>Price: {product.rating}</p>
             <p><button className='btn btn-outline-primary'><Link to={'/'}>Home</Link></button></p>
+            <p><button className='btn btn-danger' onClick={(e) => {handleDelete(e, product._id)}}>Delete</button></p>
 
         </div>
     )
