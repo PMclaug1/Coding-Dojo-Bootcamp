77e01b398ef45507280d00f5848da2821a8be8ba
PMclaug1 <pmclaug1@gmail.com>
2023-02-09 14:26:11 -0600

    update

--- Java/CafeBusinessLogic/CafeUtil.java
+++ Java/CafeBusinessLogic/CafeUtil.java
@@ -12,24 +12,31 @@ public class CafeUtil {
     return sum;
     }
 
-    // double getOrderTotal(double[] prices)
-    // public getOrderTotal(double[] prices){
-    //     double sum = 0;
-    //     for(int i = 0; i < prices.length; i++){
-    //         sum += prices[i];
-    //     }
-    //     System.out.println(sum);
-    // }
+    public double getOrderTotal(double[] prices){
+        double sum = 0;
+        for(int i = 0; i < prices.length; i++){
+            sum += prices[i];
+        }
+        System.out.println(sum);
+        return sum;
+    }
 
     // // void displayMenu(ArrayList<String> menuItems)
-    // public void displayMenu(ArrayList<String>menuItems){
-    //     for (int i = 0; i < menuItems.length; i++) {
-    //         String name = menuItems.get(i);
-    //     }
-    //     System.out.println(int name + menuItems[i]);
-    // }
+    public void displayMenu(ArrayList<String> menuItems){
+        String name = menuItems.get(0); 
+        String name1 = menuItems.get(1); 
+        String name2 = menuItems.get(2); 
+        String name3 = menuItems.get(3); 
+    }
 
-    // public void addCustomer(ArrayList<String> customerArray)
-    // customerArray.add("Heidi");
-    // String userName = System.console().readLine();
+
+
+
+    public void addCustomer(ArrayList<String> customerArray){
+    customerArray.add("Heidi");
+    customerArray.add("Paul");
+    customerArray.add("Peter");
+    customerArray.add("Mary");
+    String userName = System.console().readLine();
+    }
 }
